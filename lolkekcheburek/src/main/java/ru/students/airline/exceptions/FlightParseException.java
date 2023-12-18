c7231a2252381b1bd3bb08eaf2d085fa1ecc92c9
Anzhelika02 <92773294+Anzhelika02@users.noreply.github.com>
2023-12-18 16:25:49 +0700

    Delete lolkekcheburek directory

--- lolkekcheburek/src/main/java/ru/students/airline/exceptions/FlightParseException.java
+++
@@ -1,7 +0,0 @@
-package ru.students.airline.exceptions;
-
-public class FlightParseException extends RuntimeException{
-    public FlightParseException(String message) {
-        super(message);
-    }
-}
--- lolkekcheburek/src/main/java/ru/students/airline/persistence/repository/PassengerRepo.java
+++
@@ -1,7 +0,0 @@
-package ru.students.airline.persistence.repository;
-
-import org.springframework.data.repository.CrudRepository;
-import ru.students.airline.persistence.entity.Passenger;
-
-public interface PassengerRepo extends CrudRepository<Passenger, Long> {
-}
